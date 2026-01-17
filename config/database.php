630ec986a4a98cb6bdc57f6c91ba1efd17101b6a
arenyak12-hash <arenyak12@gmail.com>
2026-01-17 21:58:47 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "database-1.cjk0syaiwgjx.ap-southeast-2.rds.amazonaws.com";
+$user = "admin";
+$pass = "arnetta12";
 $db   = "dwu_db";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
@@ -9,3 +9,4 @@ $koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
